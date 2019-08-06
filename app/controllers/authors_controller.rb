22391b86ff0203941e9237ca060a2f880b89fa4a
Majun1990 <merijun_a@hotmail.com>
2019-08-06 19:59:45 +0000

    initial commit

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,31 +4,31 @@ class AuthorsController < ApplicationController
   # GET /authors
   # GET /authors.json
   def index
-    @author = Author.all
+    @authors = Author.all
   end
 
   # GET /authors/1
   # GET /authors/1.json
-  def shows
+  def show
   end
 
   # GET /authors/new
-  def news
-    @authors = Author.new
+  def new
+    @author = Author.new
   end
 
   # GET /authors/1/edit
-  def edits
+  def edit
   end
 
   # POST /authors
   # POST /authors.json
-  def creates
-    @author = Author.new(author_params)
+  def create
+   Author.create(author_params)
 
     respond_to do |format|
       if @author.save
-        format.html { redirect_to @author, notice: 'Author was successfully created.' }
+        format.html { redirect_to authors_path, notice: 'Author was successfully created.' }
         format.json { render :show, status: :created, location: @author }
       else
         format.html { render :new }
@@ -39,7 +39,7 @@ class AuthorsController < ApplicationController
 
   # PATCH/PUT /authors/1
   # PATCH/PUT /authors/1.json
-  def updates
+  def update
     respond_to do |format|
       if @author.update(author_params)
         format.html { redirect_to @author, notice: 'Author was successfully updated.' }
@@ -53,7 +53,7 @@ class AuthorsController < ApplicationController
 
   # DELETE /authors/1
   # DELETE /authors/1.json
-  def destroys
+  def destroy
     @author.destroy
     respond_to do |format|
       format.html { redirect_to authors_url, notice: 'Author was successfully destroyed.' }
